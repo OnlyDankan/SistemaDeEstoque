3e4d39678ea47ca159c22ea811df1c576b32e29c
Daniel <marcosdanielsferreira2015@gmail.com>
2026-07-28 11:24:15 -0300

    assembly

--- obj/Debug/net10.0/Desafios.AssemblyInfo.cs
+++ obj/Debug/net10.0/Desafios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Desafios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f1305cec67832bc8d9d37b470dc035c50f461f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c7f336988cde62090ac794d0061c8241191622a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Desafios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Desafios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
